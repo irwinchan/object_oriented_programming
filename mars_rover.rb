f7560a002579030a22cd83d3b066ef30b7ce4d33
Irwin <irwinchan@me.com>
2015-08-19 23:09:38 -0400

    Add mars rover exercise and initial set up for Rover class.

---
+++ mars_rover.rb
@@ -0,0 +1,17 @@
+class Rover 
+	def initialize (x_coordinate, y_coordinate, direction)
+		@x_coordinate = x_coordinate
+		@y_coordinate = y_coordinate
+		@direction = direction
+	end
+
+	def move
+	end
+
+	def turn
+	end
+
+	def read_instruction (instructions)
+	end
+
+end
