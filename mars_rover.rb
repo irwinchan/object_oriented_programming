4a649465a7b76012ebbb5c63dd557df874164ebc
Irwin <irwinchan@me.com>
2015-08-20 15:51:42 -0400

    Add read_instructions, move and turn methods to Rover class

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,17 +1,55 @@
-class Rover 
-	def initialize (x_coordinate, y_coordinate, direction)
+class Rover
+
+	attr_reader :direction
+
+	def initialize(x_coordinate, y_coordinate, direction)
 		@x_coordinate = x_coordinate
 		@y_coordinate = y_coordinate
 		@direction = direction
 	end
 
+	def read_instruction(instructions)
+		movements_list = instructions.split("")
+
+		movements_list.each do |movement|
+			if movement == "L" || movement == "R"
+				turn(movement)
+			elsif movement == "M"
+				move
+			end
+		end
+
+	end
+
 	def move
+		case @direction
+		when "N"
+			@x_coordinate += 1
+		when "S"
+			@x_coordinate -= 1
+		when "E"
+			@y_coordinate += 1
+		when "W"
+			@y_coordinate -= 1
+		end
 	end
 
-	def turn
-	end
+	def turn(dir)
+		compass = ["N", "E", "S", "W"]
+		compass_pointer = compass.index(@direction)
 
-	def read_instruction (instructions)
-	end
+		if dir == "L"
+			compass_pointer -= 1
+		elsif dir == "R"
+			compass_pointer += 1
+		end
 
+		if compass_pointer < 0
+			compass_pointer = 3
+		elsif compass_pointer > 3
+			compass_pointer = 0
+		end
+
+		@direction = compass[compass_pointer]
+	end
 end
