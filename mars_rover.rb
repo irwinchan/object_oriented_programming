1f322a40abb8f5c399223a93c1447206b1c87c92
Irwin <irwinchan@me.com>
2015-08-20 16:31:17 -0400

    Fix Rover move method by swapping the x-axis and y-axis.

--- mars_rover.rb
+++ mars_rover.rb
@@ -1,11 +1,13 @@
 class Rover
 
-	attr_reader :direction
-
 	def initialize(x_coordinate, y_coordinate, direction)
 		@x_coordinate = x_coordinate
 		@y_coordinate = y_coordinate
 		@direction = direction
+	end
+
+	def status
+		puts "#{@x_coordinate} #{@y_coordinate} #{@direction}"
 	end
 
 	def read_instruction(instructions)
@@ -18,19 +20,18 @@ class Rover
 				move
 			end
 		end
-
 	end
 
 	def move
 		case @direction
 		when "N"
-			@x_coordinate += 1
-		when "S"
-			@x_coordinate -= 1
-		when "E"
 			@y_coordinate += 1
-		when "W"
+		when "S"
 			@y_coordinate -= 1
+		when "E"
+			@x_coordinate += 1
+		when "W"
+			@x_coordinate -= 1
 		end
 	end
 
@@ -53,3 +54,33 @@ class Rover
 		@direction = compass[compass_pointer]
 	end
 end
+
+class Plateau
+	@length
+	@width
+	@contents
+
+	def initialize(length, width)
+		@length = length
+		@width = width
+	end
+
+	#outputs the location of objects on the plateau
+	def contents(x_coord, y_coord)
+
+	end
+
+	#checks to see if a movement will put a Rover off the board
+	def still_on?
+	end
+
+end
+
+robo1 = Rover.new(1, 2, "N")
+robo2 = Rover.new(3, 3, "E")
+
+robo1.read_instruction("LMLMLMLMM")
+robo2.read_instruction("MMRMMRMRRM")
+
+robo1.status
+robo2.status
